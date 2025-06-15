4b70d5d1c30ae21dff78b02a1b28731d2c33c8a8
bankimRabha <rabhabankim6@gmail.com>
2025-06-15 14:59:19 +0530

    Infinite Scrolling

--- Assets/Scripts/AnotherScript/ParallaxController.cs
+++ Assets/Scripts/AnotherScript/ParallaxController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ParallaxController : MonoBehaviour
 {
-    private  float _startPos; //store the initial position 
+    private  float _startPos, length; //store the initial position 
     public GameObject cam;
     public float parallaxEffect; //The speed at which the BG shoudl move relative to the camera
 
@@ -11,14 +11,26 @@ public class ParallaxController : MonoBehaviour
     {
         //save the bg starting horizontal position for later reference
         _startPos = transform.position.x; //we can set it to y if we want to move vertically
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void FixedUpdate()
     {
         //calculate distance BG move based on cam movement
         float distance = cam.transform.position.x * parallaxEffect; //0 = move with cam & 1 = doesn't move at all & 0.5 = move half way with cam
+        float movement = cam.transform.position.x * (1 - parallaxEffect);   //how far the camera has moved
 
         transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > _startPos + length)
+        {
+            _startPos += length;
+        }
+        else if (movement < _startPos - length)
+        {
+            _startPos -= length;
+        }
+
     }
 
 }
