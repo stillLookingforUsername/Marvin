4c13a746b3d6c3e76d35784918b330af292f7766
bankimRabha <rabhabankim6@gmail.com>
2025-07-25 15:12:15 +0530

    UI Button(Left,Right,Jump)

--- Assets/Scripts/AnotherScript/InputManager.cs
+++ Assets/Scripts/AnotherScript/InputManager.cs
@@ -11,6 +11,18 @@ public class InputManager : MonoBehaviour
     public static bool RunIsHeld;
     public static bool DashWasPressed;
 
+    // UI button support
+    public static bool UIMoveLeft;
+    public static bool UIMoveRight;
+    public static bool UIJumpPressed;
+
+    public void OnMoveLeftDown() { UIMoveLeft = true; }
+    public void OnMoveLeftUp() { UIMoveLeft = false; }
+    public void OnMoveRightDown() { UIMoveRight = true; }
+    public void OnMoveRightUp() { UIMoveRight = false; }
+    public void OnJumpDown() { UIJumpPressed = true; }
+    public void OnJumpUp() { UIJumpPressed = false; }
+
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
@@ -27,13 +39,24 @@ public class InputManager : MonoBehaviour
         _dashAction = PlayerInput.actions["Dash"];
     }
 
+    private static bool _uiJumpPrevFrame = false;
+
     private void Update()
     {
-        Movement = _moveAction.ReadValue<Vector2>();
+        Vector2 inputMovement = _moveAction.ReadValue<Vector2>();
+        float x = inputMovement.x;
+        if (UIMoveLeft && !UIMoveRight) x = -1;
+        if (UIMoveRight && !UIMoveLeft) x = 1;
+        if (UIMoveLeft && UIMoveRight) x = 0; // both pressed, no movement
+        Movement = new Vector2(x, 0);
 
-        JumpWasPressed = _jumpAction.WasPressedThisFrame();
-        JumpIsHeld = _jumpAction.IsPressed();
-        JumpWasReleased = _jumpAction.WasReleasedThisFrame();
+        // UI jump "was pressed this frame" logic
+        bool uiJumpWasPressedThisFrame = UIJumpPressed && !_uiJumpPrevFrame;
+        _uiJumpPrevFrame = UIJumpPressed;
+
+        JumpWasPressed = _jumpAction.WasPressedThisFrame() || uiJumpWasPressedThisFrame;
+        JumpIsHeld = _jumpAction.IsPressed() || UIJumpPressed;
+        JumpWasReleased = _jumpAction.WasReleasedThisFrame() || (!UIJumpPressed && _uiJumpPrevFrame);
 
         RunIsHeld = _runAction.IsPressed();
         DashWasPressed = _dashAction.WasPressedThisFrame();
